8ba34a62ac0a787c095b1f665dd556aed683ba8d
chunk offset <chunkoffset@gmail.com>
2015-02-08 22:44:56 +0900

    データベース書換（途中

--- script.rb
+++ script.rb
@@ -1,3 +1,92 @@
+module BeUnko
+  #-------------------
+  # Utility
+  #-------------------
+  def self.max_cyclic(org, max)
+    return (org <= max) ? org : 0
+  end
+  def self.max_stop(org, max)
+    return (org <= max) ? org : max
+  end
+  #-------------------
+  # module_function(s)
+  #-------------------
+  module_function
+  #
+  # convert string
+  #
+  def string(org)
+    serial = 0;
+    last_type = '-'
+    result = ""
+    # convert
+    org.split(//).each{|ch|
+      # ひらがな
+      if    /\p{Hiragana}/ =~ ch then
+        # 小書き文字
+        if /[ぁぃぅぇぉっゃゅょゎ]/ =~ ch then
+          replace = ['ぅ','っ','ぉ']
+          serial = ('hira'==last_type) ? serial : 0;
+        # ナミ字
+        else
+          replace = ['う','ん','こ']
+          serial = ('hira'==last_type) ? max_cyclic(serial+1,replace.length-1) : 0;
+        end
+        ch = replace[serial]
+        last_type = 'hira'
+      # カタカナ
+      elsif /\p{Katakana}/ =~ ch then
+        # 小書き文字
+        if /[ァィゥェォヵヶッャュョヮ]/ =~ ch then
+          replace = ['ゥ','ッ','ォ']
+          serial = ('kata'==last_type) ? serial : 0;
+        # 小書き文字(半角)
+        elsif /[ｧｨｩｪｫヵヶｯｬｭｮヮ]/ =~ ch then
+          replace = ['ｩ','ｯ','ｫ']
+          serial = ('kata'==last_type) ? serial : 0;
+        # ナミ字(半角)
+        elsif /[ｱ-ﾝ]/ =~ ch then
+          replace = ['ｳ','ﾝ','ｺ']
+          serial = ('kata'==last_type) ? max_cyclic(serial+1,replace.length-1) : 0;
+        # ナミ字
+        else
+          replace = ['ウ','ン','コ']
+          serial = ('kata'==last_type) ? max_cyclic(serial+1,replace.length-1) : 0;
+        end
+        ch = replace[serial]
+        last_type = 'kata'
+      # 漢字
+      elsif /[一-龠々]/ =~ ch then
+        ch = '糞'
+        last_type = 'kanji'
+      # アルファベット
+      elsif /[a-zA-Z]/ =~ ch then
+        # 小文字
+        if /[a-z]/ =~ ch then
+          replace = ['u','n','k','o']
+        # 大文字
+        else
+          replace = ['U','N','K','O']
+        end
+        serial = ('alpha'==last_type) ? max_stop(serial+1,replace.length-1) : 0;
+        ch = replace[serial]
+        last_type = 'alpha'
+      # 継続
+      elsif /[ー―～・]/ =~ ch then
+        # last_type更新しない
+      # その他
+      else
+        last_type = '-'
+      end
+      
+      result = result + ch.to_s
+    }
+    
+    return result
+  end
+end
+
+
 #==============================================================================
 # ■ Vocab
 #------------------------------------------------------------------------------
@@ -119,9 +208,51 @@ module Vocab
   def self.mp_a;        "unko";       end   # MP (短)
   def self.tp;          "Unko";       end   # TP
   def self.tp_a;        "unko";       end   # TP (短)
-  def self.cancel;      "おしっこ";   end   # やめる
   #--------------------------------------------------------------------------
 end
+#==============================================================================
+# ■ DataManager
+#------------------------------------------------------------------------------
+# 　データベースとゲームオブジェクトを管理するモジュールです。ゲームで使用する
+# ほぼ全てのグローバル変数はこのモジュールで初期化されます。
+#==============================================================================
+
+module DataManager
+  #--------------------------------------------------------------------------
+  # ● 通常のデータベースをロード
+  #--------------------------------------------------------------------------
+  def self.rename_to_unko(data)
+    if data.nil? then
+      return
+    end
+    if data.respond_to?(:name)         then data.name        = "うんこ" end
+    if data.respond_to?(:nickname)     then data.nickname    = "うんこ" end
+    data
+  end
+  class << self
+    alias :beunko_datamanager_load_normal_database :load_normal_database unless method_defined?(:beunko_datamanager_load_normal_database)
+  end
+  def self.load_normal_database
+    beunko_datamanager_load_normal_database()
+    $data_actors.map!  {|data| rename_to_unko(data)}
+    $data_classes.map! {|data| rename_to_unko(data)}
+
+  end
+  #--------------------------------------------------------------------------
+  # ● 戦闘テスト用のデータベースをロード
+  #--------------------------------------------------------------------------
+  def self.load_battle_test_database
+    $data_actors        = load_data("Data/BT_Actors.rvdata2")
+    $data_classes       = load_data("Data/BT_Classes.rvdata2")
+    $data_skills        = load_data("Data/BT_Skills.rvdata2")
+    $data_items         = load_data("Data/BT_Items.rvdata2")
+    $data_weapons       = load_data("Data/BT_Weapons.rvdata2")
+    $data_armors        = load_data("Data/BT_Armors.rvdata2")
+    $data_enemies       = load_data("Data/BT_Enemies.rvdata2")
+    $data_states        = load_data("Data/BT_States.rvdata2")
+  end
+end
+
 #==============================================================================
 # ■ Game_Actor
 #------------------------------------------------------------------------------
@@ -169,5 +300,43 @@ class Game_Troop < Game_Unit
   #--------------------------------------------------------------------------
   LETTER_TABLE_HALF = [' U',' N',' K',' O',' B',' U',' R',' I']
   LETTER_TABLE_FULL = ['Ｕ','Ｎ','Ｋ','Ｏ','Ｂ','Ｕ','Ｒ','Ｉ']
+end
+#==============================================================================
+# ■ Window_Base
+#------------------------------------------------------------------------------
+# 　ゲーム中の全てのウィンドウのスーパークラスです。
+#==============================================================================
+
+class Window_Base < Window
+  #--------------------------------------------------------------------------
+  # ● 制御文字の事前変換
+  #    実際の描画を始める前に、原則として文字列に変わるものだけを置き換える。
+  #    文字「\」はエスケープ文字（\e）に変換。
+  #--------------------------------------------------------------------------
+  alias beunko_window_base_convert_escape_characters convert_escape_characters
+  def convert_escape_characters(text)
+    result = BeUnko::string(text.to_s)
+    result = beunko_window_base_convert_escape_characters(result)
+    result
+  end
+end
+
+#==============================================================================
+# ■ Window_MapName
+#------------------------------------------------------------------------------
+# 　マップ名を表示するウィンドウです。
+#==============================================================================
+
+class Window_MapName < Window_Base
+  #--------------------------------------------------------------------------
+  # ● リフレッシュ
+  #--------------------------------------------------------------------------
+  def refresh
+    contents.clear
+    unless $game_map.display_name.empty?
+      draw_background(contents.rect)
+      draw_text(contents.rect, BeUnko::string($game_map.display_name), 1)
+    end
+  end
 end
 
